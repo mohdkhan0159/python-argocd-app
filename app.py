292e86c2533eced6d2b9a94434a5598034f08489
mohdkhan0159 <mohdkhan0159@gmail.com>
2026-02-28 17:40:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, jsonify
+import os
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "ArgoCD GitOps Demo",
+        "environment": os.getenv("ENVIRONMENT", "dev")
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
